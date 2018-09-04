a52fc25d4a12431c09c6341b42abe3980f6c7a36
Sam <wanekeyas@kakbima.com>
2018-09-04 20:28:51 +0300

    Updates
    Updated International Telephone Input Plugin
    Updated to latest jquery plugin
    Lots of bug fixes

--- js/defaultCountryIp.js
+++ js/defaultCountryIp.js
@@ -1,24 +1,20 @@
-/*
- * Obtain user location and validate phone number
- * User location tracking source: International Telephone Input v10.0.6
- */
-
- $("#phone").intlTelInput({
+$("#phone").intlTelInput({
   initialCountry: "auto",
   geoIpLookup: function(callback) {
-    $.get('//ipinfo.io', function() {}, "jsonp").always(function(resp) {
+    $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
       var countryCode = (resp && resp.country) ? resp.country : "";
       callback(countryCode);
     });
   },
-  utilsScript: "js/utils.js" // just for formatting/placeholders etc
+  utilsScript: "js/utils.js?1535108287294" // just for formatting/placeholders etc
 });
+
 /*
  * Let us validate the phone number
 */
 var telInput = $("#phone"),
   errorMsg = $("#error-msg"),
-  validMsg = $("#valid-msg");
+  validMsg = $("#valid-msg"); //Shut this on the main page cause I won't need it
 
 var reset = function() {
   telInput.removeClass("error");
